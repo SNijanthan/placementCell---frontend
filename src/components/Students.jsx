5ab4d71f6997bf84fcdbb30693a176422c5839c6
SNijanthan <nijanthan378@gmail.com>
2025-04-07 18:55:49 +0530

    UI update

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -10,35 +10,41 @@ const Students = () => {
       <h1 className="text-center mb-5 text-5xl font-light text-purple-500">
         STUDENT'S DETAILS
       </h1>
-      <>
-        <div className="overflow-x-auto w-5/6 m-auto bg-gray-900 rounded-box p-2">
-          <table className="table">
-            <thead>
-              <tr className="text-center font-mono text-amber-50">
-                <th>Name</th>
-                <th>College</th>
-                <th>DSA Score</th>
-                <th>Web Developement Score</th>
-                <th>React Score</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {students.map((student) => (
-                <tr key={student._id} className="hover:bg-base-300 text-center">
-                  <td>{student.name}</td>
-                  <td>{student.college}</td>
-                  <td>{student.dsaScore}</td>
-                  <td>{student.webDScore}</td>
-                  <td>{student.reactScore}</td>
-                  <td>{student.status}</td>
+      {students.length === 0 ? (
+        <p className="text-center text-gray-400 text-lg">No students found.</p>
+      ) : (
+        <>
+          <div className="overflow-x-auto w-5/6 m-auto bg-gray-900 rounded-box p-2">
+            <table className="table">
+              <thead>
+                <tr className="text-center font-mono text-amber-50">
+                  <th>Name</th>
+                  <th>College</th>
+                  <th>DSA Score</th>
+                  <th>Web Developement Score</th>
+                  <th>React Score</th>
+                  <th>Status</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </>
-
+              </thead>
+              <tbody>
+                {students.map((student) => (
+                  <tr
+                    key={student._id}
+                    className="hover:bg-base-300 text-center"
+                  >
+                    <td>{student.name}</td>
+                    <td>{student.college}</td>
+                    <td>{student.dsaScore}</td>
+                    <td>{student.webDScore}</td>
+                    <td>{student.reactScore}</td>
+                    <td>{student.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
       <div className="flex justify-end">
         <button className="btn bg-green-700 mr-32 mt-5">
           <Link to="/student">Add student</Link>
