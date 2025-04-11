7f819d9de642d8df251f46af05b573e87599a29b
SNijanthan <nijanthan378@gmail.com>
2025-04-11 21:15:17 +0530

    Update homepage layout

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,7 @@
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:7000" : "/api";
+
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7000" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:7000"
+    : "http://13.60.20.16:7000";
